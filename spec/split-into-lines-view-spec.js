da82cb93a97c002649b4f1d3a85fbbafba4ec30a
João André <joaoandre.ja@gmail.com>
2016-08-21 21:51:40 -0300

    First commit

---
+++ spec/split-into-lines-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import SplitIntoLinesView from '../lib/split-into-lines-view';
+
+describe('SplitIntoLinesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
