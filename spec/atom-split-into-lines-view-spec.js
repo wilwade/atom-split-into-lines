961ce53fa8ef568449e3bc76473550342e84ac31
João André <joaoandre.ja@gmail.com>
2016-08-21 21:29:44 -0300

    First commit

---
+++ spec/atom-split-into-lines-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomSplitIntoLinesView from '../lib/atom-split-into-lines-view';
+
+describe('AtomSplitIntoLinesView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
