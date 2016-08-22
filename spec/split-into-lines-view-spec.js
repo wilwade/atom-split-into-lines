74f52af356e91acfa0583ad033c641c126d1e149
João André <joaoandre.ja@gmail.com>
2016-08-22 16:03:53 -0300

    Commented outdated tests

--- spec/split-into-lines-view-spec.js
+++ spec/split-into-lines-view-spec.js
@@ -1,9 +1,9 @@
-'use babel';
-
-import SplitIntoLinesView from '../lib/split-into-lines-view';
-
-describe('SplitIntoLinesView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
+// 'use babel';
+//
+// import SplitIntoLinesView from '../lib/split-into-lines-view';
+//
+// describe('SplitIntoLinesView', () => {
+//   it('has one valid test', () => {
+//     expect('life').toBe('easy');
+//   });
+// });
